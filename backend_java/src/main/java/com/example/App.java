821d3b607cac1fd6d2ffdeccac1cda882cf1a422
fabriziojuarez <fabriziojuarez79@gmail.com>
2025-10-31 11:32:54 -0500

    feat: prueba de endpoint: retorno de metodo http

--- backend_java/src/main/java/com/example/App.java
+++ backend_java/src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.routes.RegistroRoutes;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -10,13 +11,13 @@ public class App
     public static void main( String[] args )
     {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
             // Registrar rutas
-            
+            RegistroRoutes.Routes(server);
 
             server.start();
-            System.out.println("Servidor corriendo en http://localhost:8080");
+            System.out.println("Servidor corriendo en http://localhost:8000");
         } catch (IOException e) {
             System.err.println("Error al iniciar el servidor: " + e.getMessage());
         }
--- backend_java/src/main/java/com/example/routes/RegistroRoutes.java
+++ backend_java/src/main/java/com/example/routes/RegistroRoutes.java
@@ -1,5 +1,35 @@
 package com.example.routes;
 
-public class RegistroRoutes {
-    
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+// import com.example.controllers.RegistroController;
+
+public class RegistroRoutes
+{
+
+    public static void Routes(HttpServer server) {
+        // RegistroController registroController = new RegistroController();
+
+        server.createContext("/registros", (HttpExchange exchange) -> {
+            String method = exchange.getRequestMethod();
+            enviar(exchange, 200, method);
+        });
+    }
+
+    public static void enviar(HttpExchange exchange, int status, String body) {
+        try{
+            exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+            byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+            exchange.sendResponseHeaders(status, bytes.length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(bytes);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
